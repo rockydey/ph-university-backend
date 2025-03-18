28fc70336ffa93bd21bb8ff1026e65f09d0d14f7
rockydey <rockydey077@gmail.com>
2025-03-18 09:00:33 +0600

    handle zod error complete

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,22 +1,35 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { ErrorRequestHandler } from 'express';
 import httpStatus from 'http-status';
+import { TErrorSource } from '../interface/error.interface';
+import { ZodError } from 'zod';
+import config from '../config';
+import handleZodError from '../errors/HandleZodError';
 
-const globalErrorHandler: ErrorRequestHandler = (
-  error: any,
-  req,
-  res,
-  next,
-) => {
-  const message = error.message || 'Something went wrong';
-  const statusCode = error.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  let message = error.message || 'Something went wrong!';
+  let statusCode = error.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSource = simplifiedError?.errorSource;
+  }
 
   res.status(statusCode).json({
     success: false,
     message,
-    error,
+    errorSource,
+    stack: config.node_env === 'development' ? error?.stack : undefined,
   });
   return;
 };
