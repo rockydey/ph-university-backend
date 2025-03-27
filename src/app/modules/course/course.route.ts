a9d2c73aa06299de90ae802facf432a71bb613fd
rockydey <rockydey077@gmail.com>
2025-03-27 19:32:53 +0600

    add role validation

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -2,33 +2,46 @@ import { Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { CourseValidation } from './course.validation';
 import { CourseController } from './course.controller';
+import auth from '../../middlewares/auth';
 
 const router = Router();
 
 router.post(
   '/create-course',
+  auth('admin'),
   validateRequest(CourseValidation.createCourseValidationSchema),
   CourseController.createCourse,
 );
 
-router.get('/', CourseController.getAllCourses);
+router.get(
+  '/',
+  auth('admin', 'faculty', 'student'),
+  CourseController.getAllCourses,
+);
 
-router.get('/:id', CourseController.getSingleCourse);
+router.get(
+  '/:id',
+  auth('admin', 'faculty', 'student'),
+  CourseController.getSingleCourse,
+);
 
 router.patch(
   '/:id',
+  auth('admin'),
   validateRequest(CourseValidation.updateCourseValidationSchema),
   CourseController.updateCourse,
 );
 
 router.put(
   '/:courseId/assign-faculties',
+  auth('admin'),
   validateRequest(CourseValidation.facultiesWithCourseValidationSchema),
   CourseController.assignFacultiesWithCourse,
 );
 
 router.delete(
   '/:courseId/remove-faculties',
+  auth('admin'),
   validateRequest(CourseValidation.facultiesWithCourseValidationSchema),
   CourseController.removeFacultiesWithCourse,
 );
